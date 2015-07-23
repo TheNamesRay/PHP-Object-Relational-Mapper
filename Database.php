3ad442f526bd768949fedded3c5ba97017c255dc
Beam Projects Ltd <kedi97@gmail.com>
2015-07-23 13:41:49 +0200

    Initial Commit

---
+++ Database.php
@@ -0,0 +1,124 @@
+<?php
+    
+/**
+ * Lightweight PHP ORM
+ * @version 1.0
+ *
+ * @author  Kedi Agbogre <kedi97@gmail.com>
+ * @skype   ThePoliticallyCorrect
+ * @php     5.3 or greater
+ * @license GNU GPL
+ */
+ 
+class Database
+{
+    /**
+     * Property: connection
+     *
+     * Where the PDO connection link
+     * will be stored.
+     */
+    public static $connection;
+    /**
+     * Property: stmt
+     *
+     * Where the PDO statement
+     * will be stored.
+     */
+    public static $stmt;
+
+    /**
+     * Constant: HOST
+     * Constant: USER
+     * Constant: PWD
+     * Constant: DB
+     *
+     * The MySQL connection details.
+     */    
+    const HOST = '127.0.0.1';
+    const USER = 'root';
+    const PWD = '';
+    const DB = 'store';
+    /**
+     * Function: load_database
+     *
+     * Create a link if not created
+     * already.
+     */    
+    private static function load_database()
+    {
+        $dsn = 'mysql:host=' . self::HOST . ';dbname=' . self::DB;
+        self::$connection = new PDO($dsn, self::USER, self::PWD, array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ));
+    }
+    /**
+     * Function: query
+     *
+     * Does a new query.
+     */  
+    public static function query($query)
+    {
+        if (!self::$connection)
+            self::load_database();
+        self::$stmt = self::$connection->prepare($query);
+		
+        return new static;
+    }
+    /**
+     * Function: bind
+     *
+     * Binds new keys into the 
+     * PDOStatement.
+     */  
+    public static function bind($pos, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        self::$stmt->bindValue($pos, $value, $type);
+        return new static;
+    }
+    /**
+     * Function: execute
+     *
+     * Executes the statement.
+     */  
+    public static function execute()
+    {
+		return self::$stmt->execute();	
+    }
+    /**
+     * Function: result_set
+     *
+     * PDOStatement::fetchAll()
+     */  
+    public static function result_set()
+    {
+        self::execute();
+        return self::$stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    /**
+     * Function: single
+     *
+     * PDOStatement::fetch()
+     */  
+    public static function single()
+    {
+        self::execute();
+        return self::$stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
---
+++ Model.php
@@ -0,0 +1,154 @@
+<?php
+
+/**
+ * Lightweight PHP ORM
+ * @version 1.0
+ *
+ * @author  Kedi Agbogre <kedi97@gmail.com>
+ * @skype   ThePoliticallyCorrect
+ * @php     5.3 or greater
+ * @license GNU GPL
+ */
+    
+require_once './Database.php';
+
+class Model
+{
+    /**
+     * Property: data
+     * Type: object
+     *
+     * All the table's contents should be 
+     * stored here.
+     */
+    public $data;
+    /**
+     * Property: validation
+     * Type: closure
+     *
+     * Should contain a closure which should
+     * contain different conditionals to validate
+     * the data, return false if failed.
+     */
+    public $validation;
+    /**
+     * Property: _table
+     * Type: string
+     *
+     * The table the ORM will work with
+     */
+    private $_table;
+    /**
+     * Function: __construct
+     *
+     * Set all the default values and 
+     * the requested table name.
+     *
+     * @param $p Table Name
+     */    
+    public function __construct($p)
+    {
+        $this->data   = new StdClass();
+        $this->validation = function(){};
+        $this->_table = $p;
+    }
+    /**
+     * Function: load
+     *
+     * Should be used for SELECT & UPDATE 
+     * queries only. It loads all the data.
+     * 
+     * @param $q Identifier
+     * @param $r Identifier Value
+     * @param $s What to Select (separated by commas, optional)
+     *
+     * @example load('id',$_SESSION['user_id'],'username, like_count, password')
+     */   
+    public function load($q, $r, $s = '*')
+    {
+        $t = "SELECT $s FROM {$this->_table} WHERE $q = ?";
+        $u = Database::query($t)->bind(1, $r)->single();
+        $this->data = (object) $u;
+        return $u ? true : false;
+    }
+    /**
+     * Function: delete
+     *
+     * Deletes a record from the database.
+     * You have to set the identifier to 
+     * delete that particular record.
+     *
+     * @param $v Identifier
+     *
+     * @example $m=new Model('users');
+     * @example $m->data->username='John';
+     * @example $m->delete('username');
+     */   
+    public function delete($v)
+    {
+        $t = "DELETE FROM {$this->_table}";
+        $t .= " WHERE $v = :where_$v";
+        $u = Database::query($t);
+        $u->bind("where_$v",$this->data->$v);
+        $u->execute();
+    }
+    /**
+     * Function: save
+     *
+     * Executes an UPDATE or SELECT query.
+     *
+     * @param $v Identifier (optional)
+     *
+     * @example $m=new Model('users');
+     * @example $m->data->username='John';
+     * @example $m->delete('username');
+     */   
+    public function save($v = null)
+    {
+        // run the validation
+        $q = $this->validation;
+        if ($q() === false)
+            return false;
+ 
+        // get data as array
+        $w = get_object_vars($this->data);
+        $y = '';
+        $j = '';
+        
+        // if identifier, we assume it's an UPDATE
+        if (null !== $v) {
+            foreach ($w as $z => $aa) {
+                if (end($w) !== $aa) {
+                    $y .= "$z = :update_$z, ";
+                } else {
+                    $y .= "$z = :update_$z";
+                }
+            }
+            $t = "UPDATE {$this->_table} SET $y";
+            $t .= " WHERE $v = :where_$v";
+            $u = Database::query($t);
+            $u->bind("where_$v",$this->data->$v);
+            foreach ($w as $z => $aa) {
+                $u->bind('update_' . $z, $aa);
+            }
+        // if NO identifier, we assume it's an INSERT
+        } else {
+            foreach ($w as $z => $aa) {
+                if (end($w) !== $aa) {
+                    $y .= ":insert_$z, ";
+                    $j .= $z . ', ';
+                } else {
+                    $j .= $z;
+                    $y .= ":insert_$z";
+                }
+            }
+            $t = "INSERT INTO {$this->_table}($j) VALUES ($y)";
+            $u = Database::query($t);
+            foreach ($w as $z => $aa) {
+                $u->bind('insert_' . $z, $aa);
+            }
+        }
+        // execute the final query
+        $u->execute();
+    }
+}
